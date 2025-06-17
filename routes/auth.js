f74bb888503be5ea95a9e472099caeae7f773f31
guardianprime <okongordian@gmail.com>
2025-06-17 09:42:09 +0100

    repaired the routing error

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,35 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-router.get('/signup', (req, res) => res.render('signup'));
-router.post('/signup', async (req, res) => {
-  await User.create(req.body);
-  res.redirect('/auth/login');
-});
-
-router.get('/login', (req, res) => res.render('login'));
-router.post('/login', async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
-  if (user && await user.comparePassword(req.body.password)) {
+router.get("/signup", (req, res) => res.render("signup"));
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hash });
+    await user.save();
     req.session.userId = user._id;
-    res.redirect('/tasks');
-  } else {
-    res.redirect('/auth/login');
+    res.redirect("/tasks");
+  } catch (err) {
+    next(err);
   }
 });
 
-router.get('/logout', (req, res) => {
-  req.session.destroy(() => res.redirect('/auth/login'));
+router.get("/login", (req, res) => res.render("login"));
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (user && (await user.comparePassword(req.body.password))) {
+    req.session.userId = user._id;
+    res.redirect("/tasks");
+  } else {
+    res.redirect("/auth/login");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => res.redirect("/auth/login"));
 });
 
 module.exports = router;
